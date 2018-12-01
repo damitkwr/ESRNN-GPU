f69274b601ba02577dd117e4aa52e1343b4b97b4
Kaung M. Khin <10078273+damitkwr@users.noreply.github.com>
2018-11-30 23:11:28 -0500

    Added stuff

--- DRNN.py
+++ DRNN.py
@@ -1,8 +1,6 @@
 # Heavily borrowed from https://github.com/zalandoresearch/pytorch-dilated-rnn
-
 import torch
 import torch.nn as nn
-
 
 class DRNN(nn.Module):
     def __init__(self, inp, hidden, n_layers, out_type, batch_first=False):
@@ -15,9 +13,9 @@ class DRNN(nn.Module):
             [nn.LSTM(inp, hidden) if i == 0 else nn.LSTM(hidden, hidden) for i in range(n_layers)])
 
         self.out_type = out_type
-        assert self.out_type in ['stack', 'concat']  # Make sure its like stack or concat
+        assert self.out_type in ['stack', 'concat']  # Make sure its stack or concat
 
-    def base_layer(self, inp, lstm_cell, rate):
+    def base_layer(self, inp, lstm_cell, rate, hidden):
         steps = len(inp)
         b_size = inp[0].size()[0]
         h_size = lstm_cell.hidden_size
@@ -27,6 +25,17 @@ class DRNN(nn.Module):
             dilated_inp = torch.stack([new_inp[i::rate, :, :] for i in range(rate)], 2)
         else:
             dilated_inp = torch.cat([new_inp[i::rate, :, :] for i in range(rate)], 1)
+
+        if hidden is None:
+             
+        else:
+            if self.out_type == 'stack':
+                hidden = torch.stack([hidden[i::rate, :, :] for i in range(rate)], 2)
+            else:
+                hidden = torch.cat([hidden[i::rate, :, :] for i in range(rate)], 1)
+
+
+
 
     def forward(self, inp):
         inp = inp.transpose(0, 1) if self.batch_first else inp
@@ -39,8 +48,7 @@ class DRNN(nn.Module):
         if steps % rate != 0:
             dilate_steps = steps // rate + 1
             pad = torch.zeros(dilate_steps * rate - inp.size(0), inp.size(1), inp.size(2)).cuda()
-            new_inp = torch.cat((inp, torch.autograd.Variable(
-                pad)))  # have to make it Autograd variable otherwise PyTorch might not calculate gradients
+            new_inp = torch.cat((inp, torch.autograd.Variable(pad)))  # have to make it Autograd variable otherwise PyTorch might not calculate gradients
         else:
             dilate_steps = steps // rate
             new_inp = inp
