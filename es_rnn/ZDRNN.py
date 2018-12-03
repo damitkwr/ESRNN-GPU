9b3dd37dc42c6fb95fba230b60272926478f2c04
andre <aredd@andrew.cmu.edu>
2018-12-03 15:27:59 -0500

    removes the skip connections

---
+++ es_rnn/ZDRNN.py
@@ -0,0 +1,134 @@
+import torch
+import torch.nn as nn
+import torch.autograd as autograd
+
+use_cuda = torch.cuda.is_available()
+
+
+class DRNN(nn.Module):
+
+    def __init__(self, n_input, n_hidden, n_layers, dropout=0, cell_type='GRU', batch_first=False):
+
+        super(DRNN, self).__init__()
+
+        self.dilations = [2 ** i for i in range(n_layers)]
+        self.cell_type = cell_type
+        self.batch_first = batch_first
+
+        layers = []
+        if self.cell_type == "GRU":
+            cell = nn.GRU
+        elif self.cell_type == "RNN":
+            cell = nn.RNN
+        elif self.cell_type == "LSTM":
+            cell = nn.LSTM
+        elif self.cell_type == "QRNN":
+            from torchqrnn import QRNN
+            cell = QRNN
+        else:
+            raise NotImplementedError
+
+        for i in range(n_layers):
+            if i == 0:
+                c = cell(n_input, n_hidden, dropout=dropout)
+            else:
+                c = cell(n_hidden, n_hidden, dropout=dropout)
+            layers.append(c)
+        self.cells = nn.Sequential(*layers)
+
+    def forward(self, inputs, hidden=None):
+        if self.batch_first:
+            inputs = inputs.transpose(0, 1)
+        outputs = []
+        for i, (cell, dilation) in enumerate(zip(self.cells, self.dilations)):
+            if hidden is None:
+                inputs, _ = self.drnn_layer(cell, inputs, dilation)
+            else:
+                inputs, hidden[i] = self.drnn_layer(cell, inputs, dilation, hidden[i])
+
+            outputs.append(inputs[-dilation:])
+
+        if self.batch_first:
+            inputs = inputs.transpose(0, 1)
+        return inputs, outputs
+
+    def drnn_layer(self, cell, inputs, rate, hidden=None):
+
+        n_steps = len(inputs)
+        batch_size = inputs[0].size(0)
+        hidden_size = cell.hidden_size
+
+        inputs, dilated_steps = self._pad_inputs(inputs, n_steps, rate)
+        dilated_inputs = self._prepare_inputs(inputs, rate)
+
+        if hidden is None:
+            dilated_outputs, hidden = self._apply_cell(dilated_inputs, cell, batch_size, rate, hidden_size)
+        else:
+            hidden = self._prepare_inputs(hidden, rate)
+            dilated_outputs, hidden = self._apply_cell(dilated_inputs, cell, batch_size, rate, hidden_size,
+                                                       hidden=hidden)
+
+        splitted_outputs = self._split_outputs(dilated_outputs, rate)
+        outputs = self._unpad_outputs(splitted_outputs, n_steps)
+
+        return outputs, hidden
+
+    def _apply_cell(self, dilated_inputs, cell, batch_size, rate, hidden_size, hidden=None):
+        if hidden is None:
+            if self.cell_type == 'LSTM':
+                c, m = self.init_hidden(batch_size * rate, hidden_size)
+                hidden = (c.unsqueeze(0), m.unsqueeze(0))
+            else:
+                hidden = self.init_hidden(batch_size * rate, hidden_size).unsqueeze(0)
+
+        dilated_outputs, hidden = cell(dilated_inputs, hidden)
+
+        return dilated_outputs, hidden
+
+    def _unpad_outputs(self, splitted_outputs, n_steps):
+        return splitted_outputs[:n_steps]
+
+    def _split_outputs(self, dilated_outputs, rate):
+        batchsize = dilated_outputs.size(1) // rate
+
+        blocks = [dilated_outputs[:, i * batchsize: (i + 1) * batchsize, :] for i in range(rate)]
+
+        interleaved = torch.stack((blocks)).transpose(1, 0).contiguous()
+        interleaved = interleaved.view(dilated_outputs.size(0) * rate,
+                                       batchsize,
+                                       dilated_outputs.size(2))
+        return interleaved
+
+    def _pad_inputs(self, inputs, n_steps, rate):
+        iseven = (n_steps % rate) == 0
+
+        if not iseven:
+            dilated_steps = n_steps // rate + 1
+
+            zeros_ = torch.zeros(dilated_steps * rate - inputs.size(0),
+                                 inputs.size(1),
+                                 inputs.size(2))
+            if use_cuda:
+                zeros_ = zeros_.cuda()
+
+            inputs = torch.cat((inputs, autograd.Variable(zeros_)))
+        else:
+            dilated_steps = n_steps // rate
+
+        return inputs, dilated_steps
+
+    def _prepare_inputs(self, inputs, rate):
+        dilated_inputs = torch.cat([inputs[j::rate, :, :] for j in range(rate)], 1)
+        return dilated_inputs
+
+    def init_hidden(self, batch_size, hidden_dim):
+        hidden = autograd.Variable(torch.zeros(batch_size, hidden_dim))
+        if use_cuda:
+            hidden = hidden.cuda()
+        if self.cell_type == "LSTM":
+            memory = autograd.Variable(torch.zeros(batch_size, hidden_dim))
+            if use_cuda:
+                memory = memory.cuda()
+            return (hidden, memory)
+        else:
+            return hidden
---
+++ es_rnn/Zmodel.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn as nn
+from es_rnn.ZDRNN import DRNN
+
+
+class ESRNN(nn.Module):
+    def __init__(self, num_series, config):
+        super(ESRNN, self).__init__()
+        self.config = config
+        self.num_series = num_series
+
+        init_lev_sms = []
+        init_seas_sms = []
+        init_seasonalities = []
+
+        # NEED TO ENSURE THAT THE GRADIENTS ARE ACCRUEING, IF NOT NEED TO TRY CREATING THESE PARAMETERS AS VARIABLES
+        # ANOTHER THING TO LOOK AT IS RATHER THAN INDEXING NORMALLY TO USE INDEX_SELECT METHOD ON THE TENSOR
+        #             UPDATE 2018-11-30: PARAMETERS SHOWING IN MODEL PRINT (AREDD)
+        for i in range(num_series):
+            init_lev_sms.append(nn.Parameter(torch.Tensor([0.5]), requires_grad=True))
+            init_seas_sms.append(nn.Parameter(torch.Tensor([0.5]), requires_grad=True))
+            init_seasonalities.append(nn.Parameter((torch.ones(config['seasonality']) * 0.5), requires_grad=True))
+
+        self.init_lev_sms = nn.ParameterList(init_lev_sms)
+        self.init_seas_sms = nn.ParameterList(init_seas_sms)
+        self.init_seasonalities = nn.ParameterList(init_seasonalities)
+
+        self.nl_layer = nn.Linear(config['state_hsize'], config['state_hsize'])
+        self.act = nn.Tanh()
+        self.scoring = nn.Linear(config['state_hsize'], config['output_size'])
+
+        self.logistic = nn.Sigmoid()
+
+        self.drnn1 = DRNN(self.config['input_size'] + self.config['num_of_categories'],
+                          self.config['state_hsize'],
+                          n_layers=4,
+                          cell_type='LSTM')
+
+    def forward(self, train, val, test, info_cat, idxs, add_nl_layer=False, testing=False):
+        # GET THE PER SERIES PARAMETERS
+        lev_sms = self.logistic(torch.stack([self.init_lev_sms[idx] for idx in idxs]).squeeze(1))
+        seas_sms = self.logistic(torch.stack([self.init_seas_sms[idx] for idx in idxs]).squeeze(1))
+        init_seasonalities = torch.stack([self.init_seasonalities[idx] for idx in idxs])
+
+        seasonalities = []
+        # prime seasonality
+        for i in range(self.config['seasonality']):
+            seasonalities.append(torch.exp(init_seasonalities[:, i]))
+        seasonalities.append(torch.exp(init_seasonalities[:, 0]))
+
+        if testing:
+            train = torch.cat((train, val), dim=1)
+
+        train = train.float()
+
+        levs = []
+        log_diff_of_levels = []
+
+        levs.append(train[:, 0] / seasonalities[0])
+        for i in range(1, train.shape[1]):
+            # CALCULATE LEVEL FOR CURRENT TIMESTEP TO NORMALIZE RNN
+            new_lev = lev_sms * (train[:, i] / seasonalities[i]) + (1 - lev_sms) * levs[i - 1]
+            levs.append(new_lev)
+
+            # STORE DIFFERENCE TO PENALIZE LATER
+            log_diff_of_levels.append(torch.log(new_lev / levs[i - 1]))
+
+            # CALCULATE SEASONALITY TO DESEASONALIZE THE DATA FOR RNN
+            seasonalities.append(seas_sms * (train[:, i] / new_lev) + (1 - seas_sms) * seasonalities[i])
+
+        seasonalities_stacked = torch.stack(seasonalities).transpose(1, 0)
+        levs_stacked = torch.stack(levs).transpose(1, 0)
+
+        if self.config['level_variability_penalty'] > 0:
+            sq_log_diff = torch.stack(
+                [(log_diff_of_levels[i] - log_diff_of_levels[i - 1]) ** 2 for i in range(1, len(log_diff_of_levels))])
+            loss_mean_sq_log_diff = torch.mean(sq_log_diff, dim=1)
+
+        if self.config['output_size'] > self.config['seasonality']:
+            start_seasonality_ext = seasonalities_stacked.shape[1] - self.config['seasonality']
+            seasonalities_stacked = torch.cat((seasonalities_stacked, seasonalities_stacked[:, start_seasonality_ext:]),
+                                              dim=1)
+
+        window_input = []
+        window_output = []
+        for i in range(self.config['input_size'] - 1, train.shape[1] - self.config['output_size']):
+            input_window_start = i + 1 - self.config['input_size']
+            input_window_end = i + 1
+
+            train_deseas_window_input = train[:, input_window_start:input_window_end] / seasonalities_stacked[:,
+                                                                                        input_window_start:input_window_end]
+            train_deseas_norm_window_input = (train_deseas_window_input / levs_stacked[:, i].unsqueeze(1))
+            train_deseas_norm_cat_window_input = torch.cat((train_deseas_norm_window_input, info_cat), dim=1)
+            window_input.append(train_deseas_norm_cat_window_input)
+
+            output_window_start = i + 1
+            output_window_end = i + 1 + self.config['output_size']
+
+            train_deseas_window_output = train[:, output_window_start:output_window_end] / seasonalities_stacked[:,
+                                                                                           output_window_start:output_window_end]
+            train_deseas_norm_window_output = (train_deseas_window_output / levs_stacked[:, i].unsqueeze(1))
+            window_output.append(train_deseas_norm_window_output)
+
+        input_batch = torch.cat([i.unsqueeze(0) for i in window_input], dim=0)
+        output_batch = torch.cat([i.unsqueeze(0) for i in window_output], dim=0)
+
+        out, _ = self.drnn1(input_batch)
+
+        if add_nl_layer:
+            out = self.nl_layer(out)
+            out = self.act(out)
+        out = self.scoring(out)
+
+        return out, output_batch
